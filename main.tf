cc957e4b35d45c6a6b67b787c639afcd8bc1b79d
Heinth <heinthanta36@gmail.com>
2024-08-04 00:32:41 +0700

    Linode provider added

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
+terraform {
+  required_providers {
+      linode = {
+            source = "linode/linode"
+	          version = "2.5.2"
+		      }
+		        }
+			}
+
+
 provider "linode" {
-  token = var.linode_token
+  token = "a20668c698fbbd0de8b78fce3cdafd7f405a34fa234e7add538182ef3a21e8e0"
 }
 
 resource "linode_instance" "my_instance" {
-  label = "example-instance"
+  label = "test-server"
   region = "ap-south"  # Singapore region
   image  = "linode/ubuntu20.04"
   type   = "g6-dedicated-2"  # Dedicated 4 GB plan
-  root_pass = "your-root-password"
+  root_pass = "yemonhe!n99H"
 }
 
 output "server_ip" {
